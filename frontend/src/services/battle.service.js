db45c95b65c41ed717bc2abbeb02962a558d24c5
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-08-29 01:08:48 +0530

    final chnages v01

--- frontend/src/services/battle.service.js
+++ frontend/src/services/battle.service.js
@@ -4,7 +4,12 @@ const app = axios.create({
       baseURL: import.meta.env.VITE_BASE_URI
 });
 
-
+/**
+ * invokes the battle graph with the given input
+ * @param {string} input - the prompt to be sent to the battle graph
+ * @returns {object} - the response from the battle graph
+ * @throws {Error} - if the battle graph fails to invoke
+ */
 export async function invokeBattle(input) {
       try {
             const response = await app.post(`/invoke`, { input });
